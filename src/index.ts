9235b8f11d80bcd9c7647509d4bb356b45126d94
kkdashu <wment17@gmail.com>
2026-01-29 18:12:02 +0800

    使用流式输出

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // import { Ollama } from 'ollama';
 import { createOllama } from 'ai-sdk-ollama'
-import { generateText } from 'ai'
+import { streamText } from 'ai'
 
 const CF_ACCESS_CLIENT_ID = process.env['CF-Access-Client-Id']
 const CF_ACCESS_CLIENT_SECRET = process.env['CF-Access-Client-Secret']
@@ -17,28 +17,15 @@ const model = ollama(
   { think: true }
 );
 
-const res = await generateText({
+const res = streamText({
   model,
   prompt: "你是谁",
 });
 
-console.log(res.text);
-console.log(res.reasoningText);
+for await (const chunk of res.textStream) {
+  process.stdout.write(chunk);
+}
 
-// const ollama2 = new Ollama({
-//   host: 'https://ollama.kkdashu.fun/',
-//   headers: {
-//     'CF-Access-Client-Id': CF_ACCESS_CLIENT_ID,
-//     'CF-Access-Client-Secret': CF_ACCESS_CLIENT_SECRET
-//   }
-// });
-// 
-// const res2 = await ollama2.chat({
-//   model: 'qwen3:14b',
-//   messages: [{
-//     role: 'user',
-//     content: '你是谁'
-//   }],
-//   stream: false
-// });
-// console.log(res2, res2.message);
+process.stdout.write('\n');
+
+console.log('reasoningText: ', await res.reasoningText);
